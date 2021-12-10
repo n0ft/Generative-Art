c1acccf7d9785d3d725c6df66c36e4b5f225767b
Anthony Malone <xbtbtc3.0@gmail.com>
2021-12-09 22:50:06 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>nohofoto Generative Art</p>
+        <a
+          className='App-link'
+          href='https://editor.p5js.org/n0ft/full/DsznEjN1d'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          GenArt
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
